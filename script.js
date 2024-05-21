c15ca4128cdf0c2f88fb2b11d2a2a0afb99b7ace
afifudin <27afifudin@gmail.com>
2024-05-21 09:23:39 +0700

    fix slider fuctionality, slider now affect the dimension of the grid

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
 const container = document.querySelector(".container")
 const GRID_DEFAULT = 16
+const slider = document.querySelector("#grid-value")
+const slider_value = document.querySelector('.slider-value')
+const slider_dimension = document.querySelector('#slider-dimension')
 
 let createGrid = (num)=>{
     const container = document.querySelector(".container")
     const gridSize = num**2
-
+    
     for(let i=0; i<gridSize; i++){
         const grid_item = document.createElement('div');
         grid_item.className = 'grid'
         grid_item.style.flexBasis = `calc(100%/${num})`;
         // grid_item.textContent = i
+
         container.appendChild(grid_item)
     }
 }
 
+document.addEventListener('DOMContentLoaded', ()=>{
+    createGrid(GRID_DEFAULT)
+    slider_dimension.innerText = `${slider.value} x ${slider.value}`
+})
 
-createGrid(GRID_DEFAULT)
+slider.addEventListener('change', ()=>{
+    slider_dimension.innerText = `${slider.value} x ${slider.value}`
+    container.innerHTML = ''
+    createGrid(slider.value)
+})
+
